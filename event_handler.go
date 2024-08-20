5016d380ad55ff254bab46e25f00334ba22bbe2b
Akshay Saini <91253756+akshaysaini13@users.noreply.github.com>
2024-08-20 17:15:16 +0530

    Update event_handler.gogghjj

--- event_handler.go
+++ event_handler.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Initial Cefefommit2")
+	fmt.Println("Initial Cefefddddommit2")
 	fmt.Println("commfghjitdscdddddddsctevd fromfsf sfchsssscscfefdddfwegewgewrgDAfdasfe")
 }
