0b80b90599a285f162b8e304218310fc70f17a70
Akshay Saini <91253756+akshaysaini13@users.noreply.github.com>
2024-09-26 14:25:06 +0530

    Update event_handler.go trelay

--- event_handler.go
+++ event_handler.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Initial Commit2")
+	fmt.Println("Initial Commit21")
 	fmt.Println("commit test")
 	fmt.Println("committed from ide2")
-  fmt.Println("initial commit 1")
+  	fmt.Println("initial commit 1")
 	fmt.Println("initial commit 2 on new branch")
 	fmt.Println("1st commit 3rd PR")
 	fmt.Println("LTTC test")
